45dceb1c423db6436329074291dc47b50bb5f378
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-21 10:16:06 +0530

    Create output.tf

---
+++ modules/ec2/output.tf
@@ -0,0 +1,16 @@
+output "instance_id" {
+
+  description = "ID of the EC2 instance"
+
+  value       = aws_instance.this.id
+
+}
+ 
+output "private_ip" {
+
+  description = "Private IP of the EC2 instance"
+
+  value       = aws_instance.this.private_ip
+
+}
+ 
