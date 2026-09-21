96ad9f2108ae29bac7a7e6ed58fc50ad0e84ac6b
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-21 10:19:16 +0530

    Create variable.tf

---
+++ modules/dynamodb/variable.tf
@@ -0,0 +1,24 @@
+variable "table_name" {
+
+  description = "DynamoDB table name"
+
+  type        = string
+
+}
+ 
+variable "environment" {
+
+  description = "Deployment environment"
+
+  type        = string
+
+}
+ 
+variable "project_name" {
+
+  description = "Project name"
+
+  type        = string
+
+}
+ 
