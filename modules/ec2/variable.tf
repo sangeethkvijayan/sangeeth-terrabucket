96658bcfead0683fa48f1250e7708aa3b068e906
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-21 10:15:27 +0530

    Create variable.tf

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,40 @@
+variable "instance_name" {
+
+  description = "EC2 instance name"
+
+  type        = string
+
+}
+ 
+variable "instance_type" {
+
+  description = "EC2 instance type"
+
+  type        = string
+
+}
+ 
+variable "ami_id" {
+
+  description = "EC2 AMI ID"
+
+  type        = string
+
+}
+ 
+variable "environment" {
+
+  description = "Deployment environment"
+
+  type        = string
+
+}
+ 
+variable "project_name" {
+
+  description = "Project name"
+
+  type        = string
+
+}
+ 
