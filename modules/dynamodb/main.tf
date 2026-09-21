01bb22060ebf3de1931f96e5f6bb44b5a5b1e7d0
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-21 10:18:50 +0530

    Create main.tf

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,30 @@
+resource "aws_dynamodb_table" "this" {
+
+  name         = var.table_name
+
+  billing_mode = "PAY_PER_REQUEST"
+ 
+  hash_key = "id"
+ 
+  attribute {
+
+    name = "id"
+
+    type = "S"
+
+  }
+ 
+  tags = {
+
+    Name        = var.table_name
+
+    Project     = var.project_name
+
+    Environment = var.environment
+
+    ManagedBy   = "Terraform"
+
+  }
+
+}
+ 
